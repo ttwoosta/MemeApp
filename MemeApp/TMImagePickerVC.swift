647d90ec9cd355e939c89aa03a61fb59eef3f8e8
tutong <ttwoosta@gmail.com>
2015-07-10 12:39:07 -0400

    Enable/disable bar button item pick image from camera according to source type available

--- MemeApp/TMImagePickerVC.swift
+++ MemeApp/TMImagePickerVC.swift
@@ -12,9 +12,19 @@ class TMImagePickerVC: UIViewController, UIImagePickerControllerDelegate, UINavi
     
     @IBOutlet weak var imgView: UIImageView!
 
+    @IBOutlet weak var barBtnItemPickImageCamera: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Enable/disable bar button item
+        // according to source type available
+        self.barBtnItemPickImageCamera.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
     
     @IBAction func pickImageFromAlbum(sender: AnyObject) {
